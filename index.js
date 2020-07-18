df812872b2f741bf23d8ad7051ab7d02d134e337
daniel2code <danielnwoke20@gmail.com>
2020-07-18 01:15:27 +0100

    enhanced code

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function show(){
+  let x = document.getElementsById('hide-link')
+   if(x.style.display==='none' ){
+       x.style.display=('block');
+       
+   }else{
+       x.style.display=('none');
+   
+   } 
+}
+    
